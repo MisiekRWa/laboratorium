1b2b62d89edfd335f914d79a4a270fde694ada4d
MisiekRWa <michal.dziwisz1@student.up.krakow.pl>
2020-11-24 19:03:52 +0100

    Pierwszy pomysł na przeszukanie listy pod kątem -1

--- zadanie4.py
+++ zadanie4.py
@@ -3,7 +3,11 @@ import time
 
 long_list = [randint(0, 3000) for element in range(1000000)]
 
+#1
 start = time.time()
-
-
+for element in long_list:
+    if element == -1:
+        print ("-1 Należy do long_list")
+        break
 koniec = time.time() - start
+print ("Pierwsze : " + str(koniec))
