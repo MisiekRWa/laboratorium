7548a2def7bc4726210c747ff5b8ee428619d2ff
MisiekRWa <73552067+MisiekRWa@users.noreply.github.com>
2021-01-20 19:30:39 +0100

    Delete zad8.py

--- zad8.py
+++
@@ -1,7 +0,0 @@
-try:
-    with open("jeden.txt", 'r') as plik1:
-        with open("dwa.txt", 'w') as plik2:
-            for linia in plik1:
-                plik2.write(linia)
-except IOError as ioe:
-    print("Blad! {}".format(ioe))
